04835593c6dfd294d0dc04af51ea8effb5edeed7
Mayuran <mayuranswee23@gmail.com>
2021-12-04 14:20:30 -0500

    add capability to add to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDo = document.querySelector("#tasks-to-do");
 
 
-var createTask = function(){    
+var createTask = function(event){    
+event.preventDefault()
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+//create a list item
 var listItem = document.createElement("li");
 listItem.className = "task-item";
-listItem.textContent = "New Task!";
+
+//create a div to hold the task and add to list
+var taskInfo = document.createElement("div");
+//give it a class
+taskInfo.className = "task-info";
+//add HTML content
+taskInfo.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span className='task-info'>" + taskTypeInput + "</span>";
+
+listItem.appendChild(taskInfo);
+
+//add entire list item to list
 taskToDo.appendChild(listItem);
 };
 
-buttonEl.addEventListener("click", createTask)
+formEl.addEventListener("submit", createTask)
